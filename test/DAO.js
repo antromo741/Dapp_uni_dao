fe1e1ab61f2488c7b7e2d619f0f88fbd6a38cc97
antromo741 <anthonyroma741@gmail.com>
2023-07-12 15:13:39 -0400

    add receive and add test

--- test/DAO.js
+++ test/DAO.js
@@ -9,20 +9,26 @@ const ether = tokens
 
 describe('Token', () => {
   let token, dao
+  let deployer
+  let funder
 
   beforeEach(async () => {
+    // Set up accounts
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
+    funder = accounts[1]
+    exchange = accounts[2]
+
+    // Deploy token
     const Token = await ethers.getContractFactory('Token')
     token = await Token.deploy('Dapp University', 'DAPP', '1000000')
 
-
+    // Deploy DAO
     const DAO = await ethers.getContractFactory('DAO')
     dao = await DAO.deploy(token.address, '500000000000000000000001')
 
-
-    accounts = await ethers.getSigners()
-    deployer = accounts[0]
-    receiver = accounts[1]
-    exchange = accounts[2]
+    // have funder send money to dao treasury
+    await funder.sendTransaction({ to: dao.address, value: ether(100) })
   })
 
   describe('Deployment', () => {
@@ -33,6 +39,10 @@ describe('Token', () => {
 
     it('returns quorum', async () => {
       expect(await dao.quorum()).to.equal('500000000000000000000001')
+    })
+
+    it('sends ether to the DAO treasury', async () => {
+      expect(await ethers.provider.getBalance(dao.address)).to.equal(ether(100))
     })
   })
 
